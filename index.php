dd16490b2ad6cdd5a75600eab1f204426bd039e1
jose <josanpino@gmail.com>
2020-09-11 23:33:55 +0200

    Core classes

--- index.php
+++ index.php
@@ -1,8 +1,1 @@
-<?php 
-/*
-// Requerir el archivo de la clase Bee.php
-require_once 'app/classes/Bee.php';
-
-// Ejecutar el framework bee
-Bee::fly();*/
-echo 'Hola mundo estoy funcionando...';
+<?php
