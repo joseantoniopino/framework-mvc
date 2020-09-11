c5fea4dc4384e7fc54a10a5179cbbe1dbfbb8000
jose <josanpino@gmail.com>
2020-09-12 00:32:56 +0200

    Bee class

--- app/classes/Bee.php
+++ app/classes/Bee.php
@@ -1,1 +1,70 @@
 <?php
+
+class Bee {
+    // Propiedades del framework
+    private string $framework = 'Bee Framework';
+    private string $version = '1.0.0';
+    private array $uri = [];
+
+    // La función principal que se ejecuta al instanciar nuestra clase
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Método para ejecutar cada "método" de forma subsecuente
+     */
+    private function init(): void
+    {
+        //
+    }
+
+    /**
+     * Método para iniciar la sesión en el sistema
+     */
+    private function init_session(): void
+    {
+        if (!session_start()) session_start();
+    }
+
+    /*
+     * Método para cargar la configuración del sistema
+     */
+    private function init_load_config(): void
+    {
+        $file = 'bee_config.php';
+        if (!is_file('app/config/' . $file)){
+            die(sprintf('El archivo %s no se encuentra, es requerido para %s funcione', $file, $this->framework));
+        }
+        require_once 'app/config/' . $file;
+    }
+
+    /**
+     * Métodos para cargar las funciones del sistema y del usuario
+     */
+    private function init_load_functions(): void
+    {
+        $file = 'bee_core_functions.php';
+        if (!is_file(FUNCTIONS . $file)){
+            die(sprintf('El archivo %s no se encuentra, es requerido para %s funcione', $file, $this->framework));
+        }
+        require_once FUNCTIONS . $file;
+
+        $file = 'bee_custom_functions.php';
+        if (!is_file(FUNCTIONS . $file)){
+            die(sprintf('El archivo %s no se encuentra, es requerido para %s funcione', $file, $this->framework));
+        }
+        require_once FUNCTIONS . $file;
+    }
+
+    /*
+     * Método para cargar todos los archivos de forma automática
+     */
+    private function init_autoload(): void
+    {
+        require_once CLASSES.'Db.php';
+        require_once CLASSES.'Model.php';
+        require_once CLASSES.'Controller.php';
+    }
+}
